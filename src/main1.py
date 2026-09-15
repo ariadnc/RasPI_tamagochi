8bdc320aaa113855fdd10e7f578f1f90e3f53520
Ariadna Crespo Chamorro <ariadnacrespochamorro@gmail.com>
2026-09-15 16:12:14 +0200

    Update and rename game.py to main1.py

---
+++ src/main1.py
@@ -0,0 +1,13 @@
+from tamagotchi import Tamagotchi
+from game import start_game
+
+
+def main():
+
+    tamagotchi = Tamagotchi()
+
+    start_game(tamagotchi)
+
+
+if __name__ == "__main__":
+    main()
